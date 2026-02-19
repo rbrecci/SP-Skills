ec56257e328da00081b96dfb94cd931ff5c3cdf6
Rafael Brecci de Souza <fbrecci@gmail.com>
2026-02-19 16:41:32 -0300

    Add files via upload

---
+++ SpeedTest/A1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="base.css">
+</head>
+<body>
+    <header>
+        <h1>MODULO A - SPEED TEST</h1>
+    </header>
+    <main>
+        <section class="design-container" id="container1">
+            <h2>Implementação de Design</h2>
+            <br>
+            <div class="task"><ul><li><a href="../A2/index.php">A2 - Botão colorido</a></li></ul></div>
+            <div class="task"><ul><li><a href="../A3/index.php">A3 - Galeria Acessível</a></li></ul></div>
+        </section>
+        <section class="design-container" id="container2">
+            <h2>Front End</h2>
+            <br>
+            <div class="task"><ul><li><a href="../B1/index.php">B1 - Fusão</a></li></ul></div>
+            <div class="task"><ul><li><a href="../B2/index.php">B2 - Comparação de Imagens</a></li></ul></div>
+        </section>
+        <section class="design-container" id="container3">
+            <h2>Back End</h2>
+            <br>
+            <div class="task"><ul><li><a href="../C1/index.php">C1 - Sistema de login usando arquivo JSON</a></li></ul></div>
+            <div class="task"><ul><li><a href="../C2/index.php">C2 - Correio Elegante</a></li></ul></div>
+        </section>
+    </main>
+</body>
+</html>
